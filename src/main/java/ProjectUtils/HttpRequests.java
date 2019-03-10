9d6a86bdf10968f050e4a3d7b5e26f35274d1e10
gal.i <gal.i@mycheck.co.il>
2019-03-10 08:53:45 +0200

    change the token

--- src/main/java/ProjectUtils/HttpRequests.java
+++ src/main/java/ProjectUtils/HttpRequests.java
@@ -17,7 +17,7 @@ public class HttpRequests {
 		
 		HttpResponse<JsonNode> jsonResponse = Unirest.post("https://slack.com/api/files.upload")
 			  .header("accept", "application/json")
-			  .header("Authorization", "Bearer xoxs-568837673152-571019995094-571933464530-97b95a9552e3113afd6032e1209e53a755e7b1e64e937ed662b8f35a4969f884")
+			  .header("Authorization", "Bearer xoxs-568837673152-571019995094-571926761299-20fe8de9708febc126b55420af6ea2f89b5056f743d2398f3c52f1ed44b5b813")
 			  .field("channels", "CGTLKU1DH")
 			  .field("file", new File("C:\\Users\\galif\\eclipse-workspace\\RedLionAutomation\\ScreenShots\\RD_screen.png"))
 			  .asJson();
