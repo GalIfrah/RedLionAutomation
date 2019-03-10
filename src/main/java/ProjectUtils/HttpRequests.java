5f538efb9d7073191483cc211f529ed7456e7506
gal.i <gal.i@mycheck.co.il>
2019-03-10 11:49:35 +0200

    add slack system property

--- src/main/java/ProjectUtils/HttpRequests.java
+++ src/main/java/ProjectUtils/HttpRequests.java
@@ -14,10 +14,10 @@ public class HttpRequests {
 	
 	
 	public void postFile() throws UnirestException {
-		
+		System.out.println(System.getProperty("slack"));
 		HttpResponse<JsonNode> jsonResponse = Unirest.post("https://slack.com/api/files.upload")
 			  .header("accept", "application/json")
-			  .header("Authorization", "Bearer xoxs-568837673152-571019995094-571926761299-20fe8de9708febc126b55420af6ea2f89b5056f743d2398f3c52f1ed44b5b813")
+			  .header("Authorization",System.getProperty("slack"))
 			  .field("channels", "CGTLKU1DH")
 			  .field("file", new File("C:\\Users\\galif\\eclipse-workspace\\RedLionAutomation\\ScreenShots\\RD_screen.png"))
 			  .asJson();
