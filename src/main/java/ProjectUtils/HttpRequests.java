1068c222e941cfdf0b9aed6629124aa061ce6006
gal.i <gal.i@mycheck.co.il>
2019-03-10 11:59:58 +0200

    change path

--- src/main/java/ProjectUtils/HttpRequests.java
+++ src/main/java/ProjectUtils/HttpRequests.java
@@ -19,7 +19,7 @@ public class HttpRequests {
 			  .header("accept", "application/json")
 			  .header("Authorization",System.getProperty("slack"))
 			  .field("channels", "CGTLKU1DH")
-			  .field("file", new File("https://github.com/GalIfrah/RedLionAutomation/blob/master/ScreenShots/RD_screen.png"))
+			  .field("file", new File("RedLionAutomation/blob/master/ScreenShots/RD_screen.png"))
 			  .asJson();
 	
 }
