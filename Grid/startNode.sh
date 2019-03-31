0a150eb318afef8a67899e4b594d538e72e6cbbe
gal ifrah <youremail@domain.com>
2019-03-31 13:20:34 +0300

    add Grid script to project

---
+++ Grid/startNode.sh
@@ -0,0 +1,1 @@
+java -jar selenium-server-standalone.jar -role node  -hub http://localhost:4444/grid/register -browser browserName=chrome,maxInstances=20,seleniumProtocol=WebDriver -maxSession 20 -cleanUpCycle 10000 -timeout 300
