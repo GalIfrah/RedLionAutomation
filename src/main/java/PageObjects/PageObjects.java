1efd7cdb894cb334ee9e9d8bec064332fa0ab068
gal ifrah <youremail@domain.com>
2019-04-02 11:26:20 +0300

    add minor changes

--- src/main/java/PageObjects/PageObjects.java
+++ src/main/java/PageObjects/PageObjects.java
@@ -1,23 +1,18 @@
 package PageObjects;
-
-import java.util.ArrayList;
 import java.util.List;
-
 import org.openqa.selenium.WebElement;
-
 import Infrastructure.GenericPageObject;
 import Infrastructure.Locators;
 
+
 public class PageObjects {
-	
-	public static class HomeScreen extends GenericPageObject{
-		
+
+
+
+	public static class HomePage extends GenericPageObject{
+
+
 		static final String SUT_URL = "https://www.redlion.com/";
-		static final String HELLOW_REWARDS_BUTTON  = "oktaToggleTitle";
-		static final String HELLOW_REWARDS_POP_UP = "//div[@class=\'jBox-container\']/div[2]";
-		static final String EMAIL_FIELD = "oktaToggleEmail";
-		static final String PASSWORD_FIELD = "oktaTogglePassword";
-		static final String SIGN_IN_BTN = "oktaToggleSubmit";
 		static final String LOCATION_FIELD = "globalSearchInput";
 		static final String FIRST_LOCATION = "//ul/li[@class='typeahead__item typeahead__group-cities'][1]";
 		static final String DATE_PICKER_FIELD = "searchStartDate";
@@ -25,152 +20,237 @@ public class PageObjects {
 		static final String ACTIVE_DATE_CLASS_VALUE = "PAselected PAfrom PAactive";
 		static final String CHECK_AVAILABILITY_BTN = "//button[@class='btn-primary waves-effect waves-red check-availability-button']";
 		static final String QUICK_BOOK_BTN = "//button[@class='btn-primary waves-effect waves-red'][text()='Quick Book']";
-		static final String CANCALLATION_POLICY = "//a[text()='Cancellation Policy']";
-		static final String CANCELATION_POPUP = "//div[@class='tipso_bubble top animated fade-in-down']";
-		static final String HAS_CANCELLATION_TEXT = "at least 24 hours prior to arrival to avoid a 1 night";
 
-		
+
+
 		public static void openRedLion() {
-			
+
 			driverWrapper.open(SUT_URL);
-			System.out.println("works?");
+
 		}
-		
-		
-		public static void clickOnHelloRewards() {
-			
-			driverWrapper.findElementBy(Locators.ID, HELLOW_REWARDS_BUTTON).click();
+
+
+		public static void enterLocationName(String city_to_search) {
+
+			driverWrapper.findElementBy(Locators.ID, LOCATION_FIELD).sendKeys(city_to_search);
+
 		}
-		
-		
-		public static void clickOnHelloRewardsPopup() {
-			
-			driverWrapper.findElementBy(Locators.XPATH, HELLOW_REWARDS_POP_UP).click();
-		}
-		
-		
-		public static void enterEmail() {
-			
-			driverWrapper.findElementBy(Locators.ID, EMAIL_FIELD).sendKeys("liorl@mycheck.co.il");
-		}
-		
-		
-		public static void enterPassword() {
-			
-			driverWrapper.findElementBy(Locators.ID, PASSWORD_FIELD).sendKeys("Lior2311");
-		}
-		
-		
-		public static void clickOnSignIn() {
-			
-			driverWrapper.findElementBy(Locators.ID, SIGN_IN_BTN).click();
-		}
-		
-		
-		public static void enterLocationName() {
-			
-			driverWrapper.findElementBy(Locators.ID, LOCATION_FIELD).sendKeys("New York");
-			
-		}
-		
-		
-		public static void chooseFirstLocatio() {
-			
+
+
+		public static void chooseFirstLocation() {
+
 			driverWrapper.findElementBy(Locators.XPATH, FIRST_LOCATION).click();
 		}
-		
-		
+
+
 		public static void clickOnStartDateField() {
-			
+
 			driverWrapper.findElementBy(Locators.ID, DATE_PICKER_FIELD).click();
 		}
-		
-		
+
+
 		public static List<WebElement> getDatePickerValues() {
-			
-			
+
 			return driverWrapper.findElementsBy(DATE_PICKER_VALUES);
 		}
-		
-		
+
+
 		public static int getSelectedStartDateIndex(List<WebElement> date_picker_values) {
-			
+
 			int i;
-			
+
 			int selected_date_index = 0;
-			
-			
+
+
 			for(i=0; i<date_picker_values.size(); i++) {
-			
-				
+
+
 				if (date_picker_values.get(i).getAttribute("class").equals(ACTIVE_DATE_CLASS_VALUE)) {
-						
+
 					selected_date_index = i;
-					
+
 					break;
-					
+
 				}
-				
-				else {
-					
-					System.out.println("missing selected start date");
-					
-				}
-				
+
 			}
-			
+
 			return selected_date_index;
 		}
-		
-		
+
+
 		public static void clickOnCheckAvailability() {
-			
+
 			driverWrapper.findElementBy(Locators.XPATH, CHECK_AVAILABILITY_BTN).click();
 		}
-		
-		
+
+
 		public static void clickOnQuickBook() {
-			
+
 			driverWrapper.findElementBy(Locators.XPATH, QUICK_BOOK_BTN).click();
 		}
-		
-		
-		public static String getCencelationPolicyText() throws InterruptedException {
-			
-			driverWrapper.waitForElement(3000);
-			
-			
-			driverWrapper.scrollToElement(CANCALLATION_POLICY);
-			
-			
-			driverWrapper.waitForElement(4000);
 
-			
-			driverWrapper.hover(CANCALLATION_POLICY);
-			
-			
-			return driverWrapper.findElementBy(Locators.XPATH, CANCELATION_POPUP).getText();
-			
+
+	}
+
+
+
+	public static class ConnectPage extends GenericPageObject{
+
+
+		static final String HELLO_REWARDS_BUTTON = "oktaToggleTitle";
+		static final String HELLO_REWARDS_POP_UP = "//div[@class=\'jBox-container\']/div[2]";
+		static final String EMAIL_FIELD = "oktaToggleEmail";
+		static final String PASSWORD_FIELD = "oktaTogglePassword";
+		static final String SIGN_IN_BTN = "oktaToggleSubmit";
+		static final String USER_NAME_FIELD = "username";
+
+
+
+		public static void clickOnHelloRewardsPopup() {
+
+			driverWrapper.findElementBy(Locators.XPATH, HELLO_REWARDS_POP_UP).click();
 		}
-		
-		
+
+
+		public static void clickOnHelloRewards_connectBtn() {
+
+			driverWrapper.findElementBy(Locators.ID, HELLO_REWARDS_BUTTON).click();
+		}
+
+
+		public static void enterEmail(String email) {
+
+			driverWrapper.findElementBy(Locators.ID, EMAIL_FIELD).sendKeys(email);
+		}
+
+
+		public static void enterPassword(String password) {
+
+			driverWrapper.findElementBy(Locators.ID, PASSWORD_FIELD).sendKeys(password);
+		}
+
+
+		public static void clickOnSignIn() {
+
+			driverWrapper.findElementBy(Locators.ID, SIGN_IN_BTN).click();
+
+
+			driverWrapper.waitForElement();
+		}
+
+
+		public static String getUserName(){
+
+			return driverWrapper.findElementBy(Locators.ID, USER_NAME_FIELD).getText();
+		}
+
+
+		public static void login(String email, String password){
+
+			ConnectPage.clickOnHelloRewards_connectBtn();
+
+			ConnectPage.enterEmail(email);
+
+			ConnectPage.enterPassword(password);
+
+			ConnectPage.clickOnSignIn();
+
+		}
+	}
+
+
+	public static class OrderSummeryPage extends GenericPageObject{
+
+
+		static final String CANCELLATION_POLICY = "//a[text()='Cancellation Policy']";
+		static final String CANCELLATION_POPUP = "//div[@class='tipso_bubble top animated fade-in-down']";
+		static final String HAS_CANCELLATION_TEXT = "at least 24 hours prior to arrival to avoid a 1 night";
+
+
+
+		public static String getCancellationPolicyText() throws InterruptedException {
+
+			driverWrapper.scrollToElement(CANCELLATION_POLICY);
+
+
+			driverWrapper.hover(CANCELLATION_POLICY);
+
+
+			return driverWrapper.findElementBy(Locators.XPATH, CANCELLATION_POPUP).getText();
+
+		}
+
+
 		public static boolean HasCancellation(String full_cancellation_text) {
-			
+
 			boolean has_cancellation = false;
-			
-		
+
+
 			if (full_cancellation_text.contains(HAS_CANCELLATION_TEXT)){
-				
-			
+
+
 				has_cancellation = true;
-				
-				
-				System.out.println("good meod!!!");
+
+
 			}
-			
+
 			return has_cancellation;
 		}
 
 	}
-	
+
+
+
+	public static class ConfirmationPage extends GenericPageObject{
+
+
+		static final String CONFIRMATION_FIELD = "//div[@class='conf_text_area']";
+		static final String PHONE_NUMBER_FIELD = "checkout-form-phone-number";
+		static final String POSTAL_CODE_FIELD = "checkout-form-zip-code";
+		static final String MANAGE_PAYMENT_METHODS_VIEW = "//*[@id='checkoutPayment']/div/div/header/h4";
+		static final String MANAGE_PAYMENT_METHODS_BTN = "//*[@id='myc-wallet']/div/div/div[4]/div[2]/div/span";
+		static final String ADD_NEW_CARD_BTN = "//div[@class='myc-wallet-common']/div[@id='myc-wallet-modal-outer']/div[1]/div[2]/div[1]/div[2]/ul/li[1]/span[text()='ADD NEW CARD']";
+
+
+
+		public static String getConfirmationText(){
+
+			return driverWrapper.findElementBy(Locators.XPATH, CONFIRMATION_FIELD).getText();
+		}
+
+
+		public static void enterPhoneNumber(String Phone_number){
+
+			driverWrapper.findElementBy(Locators.ID, PHONE_NUMBER_FIELD).sendKeys(Phone_number);
+
+		}
+
+
+		public static void enterPostalCode(String postal_code){
+
+			driverWrapper.findElementBy(Locators.ID, POSTAL_CODE_FIELD).sendKeys(postal_code);
+
+		}
+
+
+		public static void clickOnMangePaymentMethods(){
+
+			driverWrapper.scrollToElement(MANAGE_PAYMENT_METHODS_VIEW);
+
+
+			driverWrapper.waitForElement();
+
+
+			driverWrapper.waitForElemToBeClickable(MANAGE_PAYMENT_METHODS_BTN).click();
+		}
+
+
+		public static void clickOnAddNewCard(){
+
+			driverWrapper.waitForElemToBeClickable(ADD_NEW_CARD_BTN).click();
+
+		}
+	}
+
 }
